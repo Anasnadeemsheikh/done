1ef650c94f05d97e1302a0e461d7306921e918b2
Muhammad Anas Nadeem <anasnadeemsheikh@gmail.com>
2022-03-19 13:13:06 +0500

    ndkjfnbda

--- index.js
+++ index.js
@@ -49,4 +49,4 @@ app.post('/', async (req, res) => {
     res.send('Email Sent!')
 })
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(process.env.PORT || port, () => console.log(`Example app listening at http://localhost:${port}`))
